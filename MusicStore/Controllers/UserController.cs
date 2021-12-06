0efbd6be0bf0cb6d95f751fbba34320536016412
Marcin Czerniawski <Marcin.Czerniawski@unit4.com>
2021-12-06 18:34:17 +0100

    delete swearwords controller, add user controller

---
+++ MusicStore/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: UserController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
